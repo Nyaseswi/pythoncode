9b8eece011417bc93d22bb91ee876ee85a7f47c6
Nyaseswi <141925995+Nyaseswi@users.noreply.github.com>
2024-01-10 11:10:45 +0530

    Update endIndexNotIncluded.py

--- slicing/endIndexNotIncluded.py
+++ slicing/endIndexNotIncluded.py
@@ -3,3 +3,8 @@ part = message[6:13]
 print(part)  # This will output " yaseswi"
 part = message[6:]
 print(part)
+
+name = 'Hello yaseswi'
+print(len(name))
+part0 = "Reads the entire string:",name[0:]
+print(part0)
