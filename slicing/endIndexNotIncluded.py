e006e36b9b2be42a36c5736630259ee4b64c3409
Nyaseswi <141925995+Nyaseswi@users.noreply.github.com>
2024-01-10 11:14:19 +0530

    Update endIndexNotIncluded.py

--- slicing/endIndexNotIncluded.py
+++ slicing/endIndexNotIncluded.py
@@ -8,5 +8,7 @@ name = 'Hello yaseswi'
 print(len(name))
 part0 = "Reads the entire string:",name[0:]
 part1 = "Last index is not included:", name[0:4]
+part2 = "Reads 1st index even if not included:",name[:13]
 print(part0)
 print(part1)
+print(part2)
