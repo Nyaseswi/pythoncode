b569a924438aa06ae9c65ce0c7a728bde54ed2cd
Nyaseswi <141925995+Nyaseswi@users.noreply.github.com>
2024-01-10 11:40:34 +0530

    Update reverseAStringWithMinusone.py

--- slicing/reverseAStringWithMinusone.py
+++ slicing/reverseAStringWithMinusone.py
@@ -3,3 +3,8 @@ print(number[::-1])
 
 name = "ssv"
 print(name[::-1])
+
+name = "ssv"
+print('syntax:[start:stop:step]')
+print('[start from the end:move to the beginning:and take each character in reverse order.]')
+print(name[::-1])
