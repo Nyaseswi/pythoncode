3d346a8a189a52016883ae0dd85534ba8a7e6b62
Nyaseswi <141925995+Nyaseswi@users.noreply.github.com>
2024-01-10 11:34:58 +0530

    Update negativeSlicing.py

--- slicing/negativeSlicing.py
+++ slicing/negativeSlicing.py
@@ -3,3 +3,15 @@ list_a = [5,3,2,1,0]
 part = list_a[-5:-2] # last index is not included 
 print(part)
 
+print('Negative Slicing')
+print('The index -1 refers to the last element, -2 refers to the second-to-last element, and so on.')
+text = "Hello, World!"
+print(len(text))
+
+# Using positive indices starts with 0 index 12! bot included 
+positive_slice = text[7:12]
+print("Positive slice:", positive_slice)  # Output: World
+
+# Using negative indices
+negative_slice = text[-6:-1]
+print("Negative slice:", negative_slice)  # Output: World
