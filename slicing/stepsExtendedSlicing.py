9c2e2deff7da8c41f6ebd6e137bf9019127f3e37
Nyaseswi <141925995+Nyaseswi@users.noreply.github.com>
2024-01-10 11:19:41 +0530

    Update stepsExtendedSlicing.py

--- slicing/stepsExtendedSlicing.py
+++ slicing/stepsExtendedSlicing.py
@@ -1,3 +1,14 @@
 a = "Waterfall"
 part = a[0:13:2]
 print(part)
+
+slicingName = "Steps Slicing"
+print(slicingName)
+name = 'Namala Venkata Yaseswi'
+print(len(name))
+part0 = name[:22:2]
+part1 = name[:22:3]
+part2 = name[:22:4]
+print("2 steps means leaves one word",part0)
+print("3 steps means leaves 2 words",part1)
+print("4 steps means leaves 3 words",part2)
