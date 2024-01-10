00e435946593c77178b495b245a7d7af93735e6e
Nyaseswi <141925995+Nyaseswi@users.noreply.github.com>
2024-01-10 12:35:34 +0530

    Update typeConversion.py

--- typeConversion.py
+++ typeConversion.py
@@ -1,3 +1,14 @@
+# data types
+print('String: Yaseswi')
+print('Integer:', 10)
+print('Float:', 10.6464)
+print('Boolean:', True)
+print('List:', [1,2,3])
+print('Tuple:',(1,2,3))
+print('Set:',{1,2,3})
+print('Dictionary: dict',{'Name':'ssv'})
+
+
 print(type('yaseswi'))
 print(type(10))
 print(type(10.76809))
