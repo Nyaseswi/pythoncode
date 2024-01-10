d8536e3305f3c7de815b20641a451f6c9d500a4b
Nyaseswi <141925995+Nyaseswi@users.noreply.github.com>
2024-01-10 12:59:17 +0530

    Update stepsExtendedSlicing.py

--- slicing/stepsExtendedSlicing.py
+++ slicing/stepsExtendedSlicing.py
@@ -12,3 +12,15 @@ part2 = name[:22:4]
 print("2 steps means leaves one word",part0)
 print("3 steps means leaves 2 words",part1)
 print("4 steps means leaves 3 words",part2)
+
+// revision:
+name = 'Namala Venkata Yaseswi '
+print('Reads the entire string:',name[0:])
+print('Last index is not included:', name[0:5])
+print('Reads start index even if left empty:', name[:6])
+print('2 step extended slicing:', name[0:6:2])
+print('3 step extended slicing:', name[0:6:3])
+print('4 step extended slicing:', name[0:6:4])
+print('-1 last element:', name[-8:-1])
+print('Reverse String:', name[::-1])
+
