63d17c643b8562536ed11cf12deed898d427717a
Nyaseswi <141925995+Nyaseswi@users.noreply.github.com>
2024-01-13 15:16:28 +0530

    Update if_ellif.py

--- nestedConditions/if_ellif.py
+++ nestedConditions/if_ellif.py
@@ -8,3 +8,17 @@ elif x != 10:
     print("x is less than 10")
 else:
     print("x is equal to 10")
+
+print('elif condition')
+
+x = 10000 
+
+if x == 100:
+    print('x is not matching the given value 10')
+elif x == 40:
+    print('x is not matching')
+elif x == 10:
+    print('matching')
+else:
+    print('Invalid')
+
